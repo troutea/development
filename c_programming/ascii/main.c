283beaedc5533ed23aa7679dfa929a00b8aabf92
troutea <troutea@yahoo.co.uk>
2024-05-20 12:02:10 +0100

    addinf files from c dev

---
+++ c_programming/ascii/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+char c = 'c';
+
+// %d displays the integer value of
+// a characted
+//% c displays the actual character
+
+printf("The actual value of %c is %d" , c, c);
+return 0;
+
+}
---
+++ c_programming/float_mux/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float multiply(float a, float b) {
+      return a * b;
+
+}
+
+
+int main() {
+
+float A = 2.12, B = 3.88, product;
+
+product = multiply(A,B);
+
+printf("Product of entered number is: %.3f", product);
+
+return 0;
+
+
+}
