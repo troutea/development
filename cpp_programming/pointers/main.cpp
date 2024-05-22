94cfc5a1c9c9b6014072299a5064a620d0e6e728
troutea <troutea@yahoo.co.uk>
2024-05-22 08:55:27 +0100

    updating code

---
+++ cpp_programming/pointers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+ 
+ int num {10};
+ cout << "The value of num is: " << num << endl;
+ cout << "sizeof of num is: " << sizeof num << endl;
+ cout << "Address of num is: " << &num << endl;
+
+
+cout << endl;
+
+}
---
+++ cpp_programming/recursion/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void sum_of_digits(int n) {
+
+int x = (n % 10);
+cout << x << endl;
+
+
+}
+
+int main() {
+
+    sum_of_digits(5);
+
+    return 0;
+}
