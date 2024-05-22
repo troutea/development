0dd070f0e84e87b655a3e54fbfc341dc374ac2a7
troutea <troutea@yahoo.co.uk>
2024-05-22 11:07:10 +0100

    add my c code from g4g

---
+++ c_programming/sizeof/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+int integerType;
+char charType;
+float floatType;
+double doubleType;
+
+//Calculate and Print the size of the integer type
+printf("Size of int is: %d", sizeof(integerType));
+
+//size of charType
+printf("\n Size of char is: %d", sizeof(charType));
+
+//the size of floatType
+printf("\nSize of float is: %d", sizeof(floatType));
+
+//the size of doubleType
+printf("\nSize of double is: %d",sizeof(doubleType));
+
+
+
+
+
+}
